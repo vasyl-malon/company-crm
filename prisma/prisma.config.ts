5162989545d48a74822d7cf2617a3b5b8cf477cb
v.malon-c <v.malon-c@tamkeentech.sa>
2026-01-23 14:40:20 +0200

    setup new db

--- prisma/prisma.config.ts
+++ prisma/prisma.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
   },
   datasource: {
     url: env('DATABASE_URL'),
-    shadowDatabaseUrl: env('DIRECT_URL'),
+    // shadowDatabaseUrl: env('DIRECT_URL'),
   },
 });
